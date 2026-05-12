7d8246b881cc7a79066c7c51cb21225318fee255
vamsee333 <vamsikiran.333@gmail.com>
2026-05-12 21:46:50 +0100

    updated Score.py

--- src/Score.py
+++ src/Score.py
@@ -4,7 +4,7 @@ import joblib
 from predict import build_inference_row
 
 # declared for monkey-patching in tests
-
+# Test comment
 MODEL        = None
 FEATURE_COLS = None
 
@@ -19,7 +19,7 @@ def init():
     feat_path = model_path.replace("model.pkl", "feature_columns.json")
 
     global MODEL, FEATURE_COLS
-    
+
     MODEL        = joblib.load(model_path)
     FEATURE_COLS = json.load(open(feat_path))
     print(f"[init] Model loaded from: {model_path}")
